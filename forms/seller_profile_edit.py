fe8b73bb840a0b7f312268ac296d5eac2cc131b8
samratpro <samratpro25@gmail.com>
2025-04-13 14:59:32 +0600

    Update seller profile edit

--- forms/seller_profile_edit.py
+++ forms/seller_profile_edit.py
@@ -5,11 +5,11 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 from ui.profile_edit_ui import Ui_ProfileEdit
 
 class Profile_Edit_Form(QDialog):
-    def __init__(self, id):
+    def __init__(self, name):
         super().__init__()
         self.ui = Ui_ProfileEdit()
         self.ui.setupUi(self)  # Call setupUi to apply the design to this dialog
-        self.id = id
+        self.name = name
         self.setup_database()
         self.setup_ui()
     def setup_database(self):
@@ -20,10 +20,22 @@ class Profile_Edit_Form(QDialog):
 
     def setup_ui(self):
         session = self.Session()
-        profile = session.query(SellerProfileModel).filter_by(id=self.id).all().first()
+        profile = session.query(SellerProfileModel).filter(SellerProfileModel.seller_name == self.name).first()
         self.ui.name.setText(profile.seller_name)
         self.ui.address.setText(profile.address)
         self.ui.phone.setText(profile.phone)
+
+    def handle_entry(self):
+        name = self.ui.name.text().strip()  # Change input field
+        phone = self.ui.phone.text().strip()
+        address = self.ui.address.text().strip()
+        if not name:
+            return False, "নাম ফাঁকা থাকা যাবেনা .."
+        if not phone:
+            return False, "ফোন নম্বর ফাঁকা থাকা যাবেনা .."
+        if not address:
+            return False, "এড্রেস ফাঁকা থাকা যাবেনা.."
+        return True, None
 
 
 
